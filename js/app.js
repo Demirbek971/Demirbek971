b160da55fc50cde158efad9428b7576a6c8c57ce
Demirbek_06 <demirbek971@gmail.com>
2024-05-28 12:27:22 +0500

    bitti

--- js/app.js
+++ js/app.js
@@ -1,11 +1,25 @@
+let lastKey;
+
 window.addEventListener('keydown', function(e) {
     const key = document.querySelector(`.key[data-key="${e.keyCode}"]`);
     if (!key) return;
     key.classList.add('playing');
     const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`);
     if (!audio) return;
-    audio.currentTime = 0; // qayta o'ynashni boshlash
-    audio.play("./sounds/bir kulishing yetadi manga.m4a"); // audio faylni ijro etish
+    if (lastKey === e.keyCode) {
+        if (!audio.paused) {
+            audio.pause();
+        } else {
+            audio.currentTime = 0; // qayta o'ynashni boshlash
+            audio.play(); // audio faylni ijro etish
+        }
+    } else {
+        const audios = document.querySelectorAll('audio');
+        audios.forEach(audio => audio.pause());
+        audio.currentTime = 0; // qayta o'ynashni boshlash
+        audio.play(); // audio faylni ijro etish
+    }
+    lastKey = e.keyCode;
 });
 
 function removeTransition(e) {
