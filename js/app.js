921bee3bb060b605892f15c2aca2ff8c3538dae6
Demirbek_06 <demirbek971@gmail.com>
2024-05-28 12:21:04 +0500

    yarim

--- js/app.js
+++ js/app.js
@@ -2,6 +2,10 @@ window.addEventListener('keydown', function(e) {
     const key = document.querySelector(`.key[data-key="${e.keyCode}"]`);
     if (!key) return;
     key.classList.add('playing');
+    const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`);
+    if (!audio) return;
+    audio.currentTime = 0; // qayta o'ynashni boshlash
+    audio.play("./sounds/bir kulishing yetadi manga.m4a"); // audio faylni ijro etish
 });
 
 function removeTransition(e) {
